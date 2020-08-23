32a5577989f5e2546b923579d2026e86c393fb06
maste150hhu <mastremolten2@gmail.com>
2020-08-23 18:51:10 +0200

    update for.bat

--- example/4. loops/for.bat
+++ example/4. loops/for.bat
@@ -2,10 +2,11 @@
 title FOR
 
 :main
-set /a i=1
-:loop_start
-echo %i%
-set/a i="i+1"
-if %i% leq 20 goto loop_start
+    set /a i=1
+    :loop_start
+        echo %i%
+        set/a i="i+1"
+        if %i% leq 20 goto loop_start
+    :loop_end
 
 :main_end
