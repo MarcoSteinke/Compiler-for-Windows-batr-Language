1026280e7d8aedc1ae25f18a77a3ea57be26fa37
maste150hhu <mastremolten2@gmail.com>
2020-08-25 19:47:00 +0200

    add argument parsing

--- Batter-Interpreter/main.cpp
+++ Batter-Interpreter/main.cpp
@@ -13,9 +13,20 @@ int main(int argc, char const *argv[])
         if(argc == 2) {
             source_file_name = argv[1];
             size_t index = source_file_name.find(".");
-            compiled_file_name = source_file_name.substr(index - 1);
+            compiled_file_name = source_file_name.substr(0, index);
 
             std::cout << compiled_file_name << "\n";
+        } if(argc > 2) {
+
+            // used later for commands during compilation
+            // if arg == something  
+
+            if(argc == 3) {
+                source_file_name = argv[1];
+                compiled_file_name = argv[2];
+
+                std::cout << source_file_name << " " << compiled_file_name << "\n";
+            }          
         }
     }
 
