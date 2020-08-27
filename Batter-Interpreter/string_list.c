a77daa156131c5c13dbcfc655482a68b861e0ba4
maste150hhu <mastremolten2@gmail.com>
2020-08-27 22:38:16 +0200

    make list increment size

--- Batter-Interpreter/string_list.c
+++ Batter-Interpreter/string_list.c
@@ -18,6 +18,6 @@ void insert(string_list* string_list, char* string)
     if(string != NULL)
     {
         string_list = realloc(string_list, sizeof(char) * string_list->MAX_STRING_LENGTH * (string_list->size + 1) );
-        strcpy(*(string_list->elements + string_list->size), string);
+        strcpy(*(string_list->elements + string_list->size++), string);
     }
 }
