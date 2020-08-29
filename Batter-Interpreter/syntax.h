1f5c55a9ff90a53acb7937ccd0c31c6dd3cee106
maste150hhu <mastremolten2@gmail.com>
2020-08-29 11:15:07 +0200

    add constants for syntax

---
+++ Batter-Interpreter/syntax.h
@@ -0,0 +1,19 @@
+#ifndef syntax_collection
+#define syntax_collection
+
+
+// define syntax
+const char OPEN_SCOPE    = '{';
+const char CLOSE_SCOPE   = '}';
+const char SEMICOLON     = ';';
+const char COMMENT[2]    = "//";
+const char NEWLINE       = '\n';
+
+// define data type syntax
+const char INT_DATA_TYPE[2] = "i:";
+const char CHAR_DATA_TYPE[2] = "c:";
+const char BOOL_DATA_TYP[2] = "b:";
+const char STRING_DATA_TYPE[2] = "s:";
+const char DOUBLE_DATA_TYPE[2] = "d:";
+
+#endif
